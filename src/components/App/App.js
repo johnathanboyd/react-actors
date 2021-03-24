4830f6c45ad52d275e23895b017e95ed01836793
Johnathan <johnathanboyd5@gmail.com>
2021-03-24 07:50:22 -0500

    initial commit: components added, nothing is working yet. Ironing out bugs

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Contact />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
